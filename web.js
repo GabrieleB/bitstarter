923392a33c8bf446dd15b9d4e1f3baa480a188b4
Ubuntu <ubuntu@ip-10-164-39-124.ec2.internal>
2013-07-07 02:54:19 +0000

    Lowered the cache time from one day to twelve hours

--- web.js
+++ web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
-/* Keep the cache for one day */
-var oneDay = 86400000;
+/* Keep the cache for 12 hours */
+var oneDay = 43200000;
 
 app.use(express.compress());
 
